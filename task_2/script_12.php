437abdb06325ef64d014242610006aff1dccee22
arjunsoni999 <145103498+arjunsoni999@users.noreply.github.com>
2023-10-18 12:25:14 +0530

    Add files via upload

---
+++ task_2/script_12.php
@@ -0,0 +1,9 @@
+<!-- Write a PHP script to concatenate two strings and display the combined result. -->
+<?php
+$string1 = "Arjun ";
+$string2 = "Dhakan";
+
+$combined = $string1 . $string2;
+
+echo $combined;
+?>
